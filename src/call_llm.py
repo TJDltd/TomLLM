e43294832635bb1df17251463ff07545ddea32e9
Tom Davey <tomjackdavey@hotmail.com>
2025-07-07 21:28:15 +0100

    Add prompting to answer questions about me

--- src/call_llm.py
+++ src/call_llm.py
@@ -1,16 +1,28 @@
-from src.config import Config
+import os
 
 from pydantic_ai import Agent
 from pydantic_ai.models.gemini import GeminiModel
 from pydantic_ai.providers.google_gla import GoogleGLAProvider
 
+from src.config import Config
+from src.system_prompt import SYSTEM_PROMPT
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CV_FILE_PATH = os.path.join(BASE_DIR, 'CV-22-05-2025.txt')
+
 model = GeminiModel(
     'gemini-2.0-flash', provider=GoogleGLAProvider(api_key=Config.GEMINI_API_KEY)
 )
 
-def create_agent():
+def _load_prompt() -> str:
+    with open(CV_FILE_PATH) as file:
+        cv_content = file.read()
+
+    return SYSTEM_PROMPT + cv_content
+
+def create_agent() -> Agent:
     return Agent(
-        system_prompt='Be concise, reply with one sentence.',
+        system_prompt=_load_prompt(),
         model=model,
     )
 
